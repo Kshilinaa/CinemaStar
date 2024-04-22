928986cecb6cfbcda30d8b308284f6af0a3fe9ce
Kshilinaa <ksusha_2605@mail.ru>
2024-04-22 15:13:35 +0300

    init

---
+++ CinemaStar/CinemaStar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinemaStar
+//
+//  Created by Ксения Шилина on 22.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
